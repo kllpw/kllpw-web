337161e7bc6cab4168d2a4d7570da295913fb4d8
kll.pw <kll.pw@protonmail.com>
2019-07-18 15:24:27 +0100

    ASCII font for figlet use

---
+++ ascii/printer.go
@@ -0,0 +1,31 @@
+package ascii
+
+import "github.com/mbndr/figlet4go"
+
+var ascii, options = initASCII()
+
+func initASCII() (*figlet4go.AsciiRender, *figlet4go.RenderOptions)  {
+    ascii := figlet4go.NewAsciiRender()
+    // Adding the colors to RenderOptions
+    options := figlet4go.NewRenderOptions()
+    options.FontName = "bloody"
+    options.FontColor = []figlet4go.Color{
+	    // Colors can be given by default ansi color codes...
+        figlet4go.ColorCyan,
+    }
+    ascii.LoadFont("./static/fonts/")
+    return ascii, options
+}
+
+// RenderString returns the string in the format defined in initASCII
+func RenderString(text string) string {
+    renderStr, _ := ascii.RenderOpts(text, options)
+    return renderStr
+}
+
+// RenderStringHTML returns the string in the format defined in initASCII
+func RenderStringHTML(text string) string {
+    options.FontColor = []figlet4go.Color{}
+	renderStr, _ := ascii.RenderOpts(text, options)
+    return "<pre>"+ renderStr +"</pre>"
+}
