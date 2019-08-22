c4bf9552d3732106d6912e94be6d47ab3a0ebd91
kll.pw <kll.pw@protonmail.com>
2019-08-22 21:24:30 +0100

    Removed RenderStringHTML

--- ascii/printer.go
+++ ascii/printer.go
@@ -9,10 +9,7 @@ func initASCII() (*figlet4go.AsciiRender, *figlet4go.RenderOptions) {
 	// Adding the colours to RenderOptions
 	options := figlet4go.NewRenderOptions()
 	options.FontName = "bloody"
-	options.FontColor = []figlet4go.Color{
-		figlet4go.ColorCyan,
-	}
-	ascii.LoadFont("./static/fonts/")
+	ascii.LoadFont("./ascii/fonts/")
 	return ascii, options
 }
 
@@ -20,11 +17,4 @@ func initASCII() (*figlet4go.AsciiRender, *figlet4go.RenderOptions) {
 func RenderString(text string) string {
 	renderStr, _ := ascii.RenderOpts(text, options)
 	return renderStr
-}
-
-// RenderStringHTML returns the string in the format defined in initASCII
-func RenderStringHTML(text string) string {
-	options.FontColor = []figlet4go.Color{}
-	renderStr, _ := ascii.RenderOpts(text, options)
-	return "<pre>" + renderStr + "</pre>"
 }
